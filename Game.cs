8afc7362cbf0b831ee071eebc0c9c87c7190aede
Brian Amerson <bmamerson@gmail.com>
2025-02-14 15:02:16 -0700

    Built out Map class; Renamed Program to Game

---
+++ Game.cs
@@ -0,0 +1,12 @@
+﻿namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+            
+            int area = map.Width * map.Height;
+        }
+    }
+}
